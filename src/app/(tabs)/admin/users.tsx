4d39f1ab6517479d350b66346a0d9e228b275b35
Sofia Marcano <soamaia03@gmail.com>
2026-09-09 23:51:26 -0400

    feat(admin-users): connected admin-users page to the correct menu and added search bar

--- src/app/(tabs)/admin/users.tsx
+++ src/app/(tabs)/admin/users.tsx
@@ -1,7 +1,151 @@
-import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useTranslation } from 'react-i18next';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Platform, StyleSheet, Image, TextInput, Pressable} from 'react-native';
+import { ThemedView } from '@/components/themed-view';
+import { ThemedText } from '@/components/themed-text';
+import { Colors, Fonts, Spacing } from '@/constants/theme';
+import { SearchFilter } from '@/components/search-filter-selector';
+import { AppHeader } from '@/components/app-header';
+import { Breadcrumb } from '@/components/breadcrumb';
+import { useTheme } from '@/hooks/use-theme';
+import { UserCard } from '@/components/user-card';
+import { firestore } from '@/config/firebase';
 
-import { ComingSoonScreen } from '@/components/navigation/coming-soon-screen';
+export default function AdminUserList() {
+    const { t } = useTranslation();
+    const theme = useTheme();
+    const styles = createStyles(theme);
 
-export default function AdminUsersScreen() {
-  return <ComingSoonScreen titleKey="navigation.adminUsers" />;
+    const [ users, setUsers ] = useState<any[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [isFromCache, setIsFromCache] = useState(false);
+
+    useEffect(() => {
+        const userList = firestore().collection('usuarios')
+          .onSnapshot(
+            (snapshot) => {
+              const data = snapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+              }));
+              setUsers(data);
+              setIsFromCache(snapshot.metadata.fromCache);
+              setLoading(false);
+            },
+            (error) => {
+              console.error("Error fetching users: ", error);
+              setLoading(false);
+            }
+          );
+
+        return () => userList()
+    }, []);
+
+    return (
+        <ThemedView style={styles.container}>
+            <AppHeader />
+            <Breadcrumb parent={t('admin.path')} current={t('admin-users.path')} />
+              <View>
+
+              {/* Title Section */}
+              <View style={styles.titleSection}>
+                <ThemedText style={styles.mainTitle}>{t('admin-users.title')}</ThemedText>
+                <ThemedText style={styles.subtitle}>
+                  {t('admin-users.subtitle')}
+                </ThemedText>
+              </View>
+
+
+              {/* Filter section */}
+              <SearchFilter></SearchFilter>
+
+              {/* List of users*/}
+              {users.map((user: userList) => (
+                  <UserCard key = {user.id}
+                    ID="#P-0042"
+                    name={user.nombre}
+                    email= {user.email}
+                    type='general'
+                    status={user.estado}
+                    role= {user.rol}
+                  />
+              ))}
+
+              {/*Selection of result pages*/}
+
+              </View>
+        </ThemedView>
+    );
 }
+
+const createStyles = (theme: any) => StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  scrollContent: {
+    paddingBottom: Platform.OS === 'ios' ? 100 : 80,
+  },
+  titleSection: {
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+  },
+  mainTitle: {
+    fontSize: 24,
+    fontWeight: '700',
+    color: theme.pageTitle, // Ebony Clay
+    fontFamily: 'Open Sans',
+    marginBottom: 6,
+  },
+  subtitle: {
+    fontSize: 14,
+    color: theme.pageSubtitle, // Pale Sky
+    lineHeight: 20,
+    fontFamily: 'Open Sans',
+  },
+    filterContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        gap: 8,
+        paddingHorizontal: 16,
+        marginBottom: 16,
+    },
+    filterLabels: {
+        color: theme.textNames,
+        fontWeight: 600,
+        fontSize: 12,
+        lineHeight: 20,
+        fontFamily: 'Open Sans',
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderWidth: 1,
+        borderColor: theme.border,
+        backgroundColor: theme.backgroundElement,
+        borderRadius: 8,
+        paddingLeft: Spacing.two || 6,
+        paddingRight: 4,
+        height: 28,
+    },
+    searchContainer: {
+        width: '70%',
+    },
+    input: {
+        flex: 1,
+        height: '100%',
+        fontSize: 12,
+        paddingVertical: 0,
+        color: theme.text,
+        fontFamily: Fonts.regular || 'System'
+    },
+    icon: {
+        margin: Spacing.two || 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+        tintColor: theme.placeholderColor
+    },
+    orderByContainer: {
+        width: '25%',
+    }
+});
---
+++ src/components/search-filter-selector.tsx
@@ -0,0 +1,114 @@
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useTranslation } from 'react-i18next';
+import React, { useMemo, useState, useEffect } from 'react';
+import { View, Text, Platform, StyleSheet, Image, TextInput, Pressable} from 'react-native';
+import { ThemedView } from '@/components/themed-view';
+import { ThemedText } from '@/components/themed-text';
+import { DropdownSelector } from '@/components/ui/dropdown-selector.tsx';
+import { Colors, Fonts, Spacing } from '@/constants/theme';
+import { useTheme } from '@/hooks/use-theme';
+
+const SearchIcon = require('@/assets/icons/search.png');
+
+type ListType = {
+    general?: boolean
+};
+
+export function SearchFilter({general= true}: ListType) {
+    const { t } = useTranslation();
+    const theme = useTheme();
+    const styles = createStyles(theme);
+
+    const orderByOptions = useMemo(() => {
+       return general
+           ? [
+               t('admin-users.orderByName'),
+               t('admin-users.orderByLastName'),
+               t('admin-users.orderByID')
+             ]
+           : [
+               t('admin-users.orderByName'),
+               t('admin-users.orderByLastName'),
+               t('admin-users.orderByID'),
+               t('patients-list.orderByLastVisit'),
+               t('patients-list.orderByNextVisit')
+             ];
+    }, [general, t]);
+
+    return (
+        <View style = {styles.filterContainer}>
+          <View style = {styles.searchContainer}>
+              <Text style = {styles.filterLabels}>
+                {general ? t('admin-users.searchUsers') : t('patients-list.searchPatients')}
+              </Text>
+              <View style = {styles.inputContainer}>
+                  <Image
+                      source={SearchIcon}
+                      style={styles.icon}
+                    />
+                  <TextInput
+                      placeholder={t('admin-users.searchUserPlaceholder')}
+                      placeholderTextColor= {theme.placeholderColor}
+                      style={styles.input}
+                    />
+              </View>
+          </View>
+          <View style = {styles.orderByContainer}>
+              <Text style = {styles.filterLabels}>
+                  {t('admin-users.orderBy')}
+              </Text>
+              <DropdownSelector
+                children = {orderByOptions}
+              ></DropdownSelector>
+          </View>
+        </View>
+    );
+}
+
+const createStyles = (theme: any) => StyleSheet.create({
+    filterContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        gap: 8,
+        paddingHorizontal: 16,
+        marginBottom: 16,
+    },
+    filterLabels: {
+        color: theme.textNames,
+        fontWeight: '600',
+        fontSize: 12,
+        lineHeight: 20,
+        fontFamily: 'Open Sans',
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderWidth: 1,
+        borderColor: theme.border,
+        backgroundColor: theme.backgroundElement,
+        borderRadius: 8,
+        paddingLeft: Spacing.two || 6,
+        paddingRight: 4,
+        height: 28,
+    },
+    searchContainer: {
+        width: '70%',
+    },
+    input: {
+        flex: 1,
+        height: '100%',
+        fontSize: 12,
+        paddingVertical: 0,
+        color: theme.text,
+        fontFamily: Fonts.regular || 'System'
+    },
+    icon: {
+        margin: Spacing.two || 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+        tintColor: theme.placeholderColor
+    },
+    orderByContainer: {
+        width: '25%',
+    }
+});
